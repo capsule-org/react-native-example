777a4fc9c856ba904ac742ef0eb14f9fdb4ad053
Taylor Bosch <43397354+tbosch82@users.noreply.github.com>
2024-02-12 11:23:12 -0600

    update app heading

--- BareExpo/src/clients/capsule.ts
+++ BareExpo/src/clients/capsule.ts
@@ -1,10 +1,6 @@
 import { CapsuleMobile, Environment } from "@usecapsule/react-native-wallet";
 
-export const capsule = new CapsuleMobile(
-  Environment.SANDBOX,
-  "8ee2d015fbc6062a6e30bdc472f2946c",
-  {
-    offloadMPCComputationURL:
-      "https://partner-mpc-computation.sandbox.usecapsule.com",
-  }
-);
+export const capsule = new CapsuleMobile(Environment.SANDBOX, "YOUR_API_KEY", {
+  offloadMPCComputationURL:
+    "https://partner-mpc-computation.sandbox.usecapsule.com",
+});
